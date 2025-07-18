584289f33a943787da8b6bfd39e3df9102dbefab
Juliusz Sosinowicz <juliusz@wolfssl.com>
2025-07-18 18:26:23 +0200

    Fix uninit variable errors

--- src/crypto.c
+++ src/crypto.c
@@ -7141,6 +7141,7 @@ CK_RV C_DeriveKey(CK_SESSION_HANDLE hSession,
             derivedKey = (byte*)XMALLOC(keyLen, NULL, DYNAMIC_TYPE_TMP_BUFFER);
             if (derivedKey == NULL)
                 return CKR_DEVICE_MEMORY;
+            XMEMSET(derivedKey, 0, keyLen);
 
             ret = WP11_EC_Derive(params->pPublicData,
                                        (int)params->ulPublicDataLen, derivedKey,
@@ -7178,6 +7179,7 @@ CK_RV C_DeriveKey(CK_SESSION_HANDLE hSession,
             derivedKey = (byte*)XMALLOC(keyLen, NULL, DYNAMIC_TYPE_TMP_BUFFER);
             if (derivedKey == NULL)
                 return CKR_DEVICE_MEMORY;
+            XMEMSET(derivedKey, 0, keyLen);
 
             ret = WP11_KDF_Derive(session, kdfParams, derivedKey, &keyLen, obj);
 
@@ -7197,6 +7199,7 @@ CK_RV C_DeriveKey(CK_SESSION_HANDLE hSession,
             derivedKey = (byte*)XMALLOC(keyLen, NULL, DYNAMIC_TYPE_TMP_BUFFER);
             if (derivedKey == NULL)
                 return CKR_DEVICE_MEMORY;
+            XMEMSET(derivedKey, 0, keyLen);
 
             ret = WP11_Dh_Derive((unsigned char*)pMechanism->pParameter,
                                     (int)pMechanism->ulParameterLen, derivedKey,
@@ -7222,6 +7225,7 @@ CK_RV C_DeriveKey(CK_SESSION_HANDLE hSession,
             derivedKey = (byte*)XMALLOC(keyLen, NULL, DYNAMIC_TYPE_TMP_BUFFER);
             if (derivedKey == NULL)
                 return CKR_DEVICE_MEMORY;
+            XMEMSET(derivedKey, 0, keyLen);
 
             ret = WP11_AesCbc_DeriveKey(params->pData, (word32)params->length,
                     derivedKey, params->iv, obj);
@@ -7256,6 +7260,7 @@ CK_RV C_DeriveKey(CK_SESSION_HANDLE hSession,
             derivedKey = (byte*)XMALLOC(keyLen, NULL, DYNAMIC_TYPE_TMP_BUFFER);
             if (derivedKey == NULL)
                 return CKR_DEVICE_MEMORY;
+            XMEMSET(derivedKey, 0, keyLen);
             ret = WP11_Tls12_Master_Key_Derive(&tlsParams->RandomInfo,
                                                tlsParams->prfHashMechanism,
                                                "key expansion", 13,
@@ -7302,6 +7307,7 @@ CK_RV C_DeriveKey(CK_SESSION_HANDLE hSession,
             derivedKey = (byte*)XMALLOC(keyLen, NULL, DYNAMIC_TYPE_TMP_BUFFER);
             if (derivedKey == NULL)
                 return CKR_DEVICE_MEMORY;
+            XMEMSET(derivedKey, 0, keyLen);
 
             ret = WP11_Tls12_Master_Key_Derive(&prfParams->RandomInfo,
                                                prfParams->prfHashMechanism,
@@ -7330,6 +7336,7 @@ CK_RV C_DeriveKey(CK_SESSION_HANDLE hSession,
             derivedKey = (byte*)XMALLOC(keyLen, NULL, DYNAMIC_TYPE_TMP_BUFFER);
             if (derivedKey == NULL)
                 return CKR_DEVICE_MEMORY;
+            XMEMSET(derivedKey, 0, keyLen);
 
             ret = WP11_Nss_Tls12_Master_Key_Derive(nssParams->pSessionHash,
                                                    nssParams->ulSessionHashLen,
